388b334b6d6564960bbba89763d061da7cb9f411
Hiromasa <howzheng0629@gmail.com>
2022-12-28 18:47:48 +0800

    .

--- main.d
+++
@@ -1,3 +0,0 @@
-main.o: main.cpp ../includes/vector.hpp ../includes/container.hpp \
-  ../includes/iterators.hpp ../includes/map.hpp ../includes/trees.hpp \
-  ../includes/stack.hpp tests.hpp colors.hpp tests.tpp
